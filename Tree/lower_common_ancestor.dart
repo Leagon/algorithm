b72a526ce50caf1a2c2b35df31661d37f0e47fb7
coverme <yunlong.gu@coverme.ws>
2019-08-16 09:40:57 +0800

    add lower common ancestor for binary search tree

--- Tree/lower_common_ancestor.dart
+++ Tree/lower_common_ancestor.dart
@@ -33,7 +33,7 @@ import 'TreeNode.dart';
 */
 TreeNode ans;
 
-TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+TreeNode lowestCommonAncestor_binary_tree(TreeNode root, TreeNode p, TreeNode q) {
   recurse(root, p, q);
   return ans;
 }
@@ -53,14 +53,58 @@ bool recurse(TreeNode root, TreeNode p, TreeNode q) {
   return (mid + left + right) > 0;
 }
 
+
+/* 
+
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先
+示例 1:
+
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+输出: 6 
+解释: 节点 2 和节点 8 的最近公共祖先是 6。
+示例 2:
+
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+输出: 2
+解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+*/
+
+TreeNode lowestCommonAncestor_binary_search_tree(TreeNode root, TreeNode p, TreeNode q) {
+
+  int pVal = p.val;
+  int qVal = q.val;
+
+  while(root != null) {
+    int rootVal = root.val;
+    if (rootVal < pVal && rootVal < qVal) {
+      root = root.right;
+    } else if (rootVal > pVal && rootVal > qVal) {
+      root = root.left;
+    } else {
+      return root;
+    }
+  }
+  return null;
+}
+
+
 void main () {
 
   BinaryTree tree = BinaryTree([3,5,1,6,2,0,8,null,null,7,4]);
   print(tree.seqTraverse());
 
-  TreeNode rs = lowestCommonAncestor(tree.root, TreeNode(5), TreeNode(1));
+  TreeNode rs = lowestCommonAncestor_binary_tree(tree.root, TreeNode(5), TreeNode(1));
   print(rs.val);
 
-  TreeNode rs2 = lowestCommonAncestor(tree.root, TreeNode(5), TreeNode(4));
+  TreeNode rs2 = lowestCommonAncestor_binary_tree(tree.root, TreeNode(5), TreeNode(4));
   print(rs2.val);
+
+  BinaryTree bst = BinaryTree([6,2,8,0,4,7,9,null,null,3,5]);
+  print(bst.seqTraverse());
+
+  TreeNode rs3 = lowestCommonAncestor_binary_search_tree(bst.root, TreeNode(2), TreeNode(8));
+  print(rs3.val);
+
+  TreeNode rs4 = lowestCommonAncestor_binary_search_tree(bst.root, TreeNode(2), TreeNode(4));
+  print(rs4.val);
 }
